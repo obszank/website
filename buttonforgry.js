57e2692a9b1e1f07a0aafb7b9cfaca72e9aa915a
obszank <obszank@gmail.com>
2024-08-17 11:21:23 +0200

    oby dzialalo v2
    plssssss

--- buttonforgry.js
+++ buttonforgry.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("redirectButton");
 
     button.addEventListener("click", () => {
-        window.location.href = 'index.html';
+        window.location.href = '2.html';
     });
 });
