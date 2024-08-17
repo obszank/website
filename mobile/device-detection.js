b653436c4f42f659e0bb11e4deba8c7cf14fe4a2
obszank <obszank@gmail.com>
2024-08-17 16:13:04 +0200

    Redirect Loop usuniete i pomylilem \ z /

--- mobile/device-detection.js
+++ mobile/device-detection.js
@@ -6,9 +6,7 @@ function isMobileDevice() {
 // Get the current page's path
 var currentPage = window.location.pathname;
 
-// Determine the mobile or PC version URL based on the current page
-if (isMobileDevice()) {
+// Avoid redirection loop by checking if already redirected
+if (!window.location.pathname.startsWith('/mobile') && isMobileDevice()) {
     window.location.href = "/mobile" + currentPage;
-} else {
-    window.location.href = currentPage; // Stay on the current page for PC version
 }
